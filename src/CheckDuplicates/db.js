455e4496f6b97567197f288d9ccb08dd57e08558
Connor J deBoer <connor.deboer04@gmail.com>
2026-06-17 02:10:45 -0600

    Added a tiny metadata table so I don't send multiple emails a day

--- src/CheckDuplicates/db.js
+++ src/CheckDuplicates/db.js
@@ -14,6 +14,27 @@ db.exec(`
     )
 `);
 
+db.exec(`
+    CREATE TABLE IF NOT EXISTS metadata (
+        key TEXT PRIMARY KEY,
+        value TEXT
+    )
+`);
+
+function GetTodayString() {
+    const now = new Date();
+    return `${now.getFullYear()}-${String(now.getMonth() + 1).padStart(2, "0")}-${String(now.getDate()).padStart(2, "0")}`;
+}
+
+export function SentToday() {
+    const row = db.prepare("SELECT value FROM metadata WHERE key = 'last_run'").get();
+    return row ? row.value == GetTodayString() : false;
+}
+
+export function SetSentToday() {
+    db.prepare(`INSERT OR REPLACE INTO metadata (key, value) VALUES ('last_run', ?)`).run(GetTodayString());
+}
+
 export function AlreadySent(id) {
     // select 1 instead of * because we only need to find 1 to know if it's already been sent
     const row = db.prepare('SELECT 1 FROM apps_sent WHERE id = ?').get(id);
